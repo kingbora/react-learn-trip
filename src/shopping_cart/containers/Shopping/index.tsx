7feb9f9060311a2d0e70dc9da17c6160ed8647ca
wenbo.kuang <wenbo.kuang@hand-china.com>
2019-07-24 22:07:03 +0800

    update: 更新购物车样式，添加注释信息

--- src/shopping_cart/containers/Shopping/index.tsx
+++ src/shopping_cart/containers/Shopping/index.tsx
@@ -3,9 +3,9 @@
  */
 import * as React from "react";
 import { connect } from "react-redux";
-import { List, Layout, Row, Col, Icon, Avatar, Button } from "antd";
+import { List, Layout, Row, Col, Icon, Avatar, Button, Affix, Drawer } from "antd";
 import style from "./style.scss";
-const { Sider, Content } = Layout;
+const { Header, Content } = Layout;
 
 interface ShoppingOwnProps { }
 interface ShoppingStateProps {
@@ -13,7 +13,9 @@ interface ShoppingStateProps {
     shoppingCart?: any;
 }
 interface ShoppingDispatchProps { }
-interface ShoppingState { }
+interface ShoppingState {
+    isShowCart: boolean;
+}
 
 @(connect(
     (state: any) => ({
@@ -27,25 +29,33 @@ interface ShoppingState { }
 export default class Shopping extends React.Component<ShoppingOwnProps & ShoppingStateProps & ShoppingDispatchProps, ShoppingState> {
     constructor(props: ShoppingOwnProps & ShoppingStateProps & ShoppingDispatchProps) {
         super(props);
+        this.state = {
+            isShowCart: false
+        };
     }
 
+    // 添加进购物车
     addToCart = (item: any, index: any) => {
 
     };
 
+    // 从购物车中移除
     removeFromCart = (item: any, index: any) => {
 
     };
 
+    // 商品列表物品详情
     renderItem = (item: any, index: number) => {
         return (
-            <List.Item actions={[<a onClick={() => this.addToCart(item, index)}>
-                <Icon type="plus-circle" />
-            </a>,
-            <span>0</span>,
-            <a onClick={() => this.removeFromCart(item, index)}>
-                <Icon type="minus-circle" />
-            </a>]}>
+            <List.Item actions={[
+                <a onClick={() => this.removeFromCart(item, index)}>
+                    <Icon type="minus-circle" />
+                </a>,
+                // 计算该物品添加的次数
+                <span>0</span>,
+                <a onClick={() => this.addToCart(item, index)}>
+                    <Icon type="plus-circle" theme="filled" />
+                </a>]}>
                 <List.Item.Meta
                     avatar={
                         <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
@@ -58,41 +68,76 @@ export default class Shopping extends React.Component<ShoppingOwnProps & Shoppin
         );
     };
 
+    // 购物车中物品详情
+    renderShoppingCartItem = (item: any, index: number) => {
+        return (
+            <div></div>
+        );
+    }
+
+    // 清除购物车中所有商品
     onClearAll = () => {
 
     };
 
+    // 打开购物车抽屉
+    openShoppingCartDrawer = () => {
+        this.setState({
+            isShowCart: true
+        });
+    };
+
+    closeShoppingCardDrawer = () => {
+        this.setState({
+            isShowCart: false
+        });
+    };
+
+    renderDrawerHeader = () => {
+        return (
+            <div className={style.rowStyle}>
+                <span className={style.greyText}>已选商品</span>
+                <div onClick={this.onClearAll} className={style.clearAll}>
+                    <Icon type="delete" /><span style={{marginLeft: '4px', color: '#333'}}>清空</span>
+                </div>
+            </div>
+        )
+    };
+
     render() {
         const { dataList, shoppingCart } = this.props;
+        const { isShowCart } = this.state;
         return (
             <Layout className={style.shoppingCart}>
-                <Content>
+                <Header className={style.whiteText}>商品列表</Header>
+                <Content className={style.shoppingList}>
                     <List
                         dataSource={dataList}
                         renderItem={this.renderItem}
                     />
                 </Content>
-                <Sider className={style.cartList}>
-                    <p>
-                        <div className={style.rowStyle}>
-                            <div>已选商品</div>
-                            <div>
-                                <Button onClick={this.onClearAll}>
-                                    <Icon type="delete" />清空
-                                </Button>
-                            </div>
-                        </div>
-                        <List
-                            dataSource={shoppingCart}
-                        />
-                    </p>
-                    <p>
-                        <div className={style.rowStyle}>
-                            <div>共0件</div>
-                            <div>总金额：¥0</div>
-                        </div>
-                    </p>
-                </Sider>
+                <div className={style.shoppingCartFixed}>
+                    <Button onClick={this.openShoppingCartDrawer} className={style.shoppingCartBtn}>
+                        <Icon type="shopping-cart" />
+                    </Button>
+                    <div className={style.totalInfo}>
+                        <div>共0件</div>
+                        <div>总金额：¥0</div>
+                    </div>
+                </div>
+                {/* 存放已选商品列表 */}
+                <Drawer
+                    title={this.renderDrawerHeader()}
+                    placement="bottom"
+                    closable={false}
+                    maskClosable={true}
+                    onClose={this.closeShoppingCardDrawer}
+                    visible={isShowCart}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Drawer>
             </Layout>
         );
     }
